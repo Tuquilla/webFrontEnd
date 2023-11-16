e2a5a10d85eb06468ce77398e1f2ff0e3658524c
Kilian Glaus <kilian.glaus@ost.ch>
2023-11-16 17:49:15 +0100

    :fire: Remove comment

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ import {useState} from 'react'
 function App() {
   const [cards, setCard] = useState<Card[]>([])
   const [sortStates, setSort] = useState<SortState>({front: true, asc:true})
-  /*const [filterTable, setFilter] = useState<boolean>(true)*/
   const [filterStates, setFilterStates] = useState<FilterState>({front: '', back: '', filterTable: true})
   return (
   <>
